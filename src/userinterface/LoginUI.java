22fd23ff3c4908413a50c17f242f7b19fb19f11a
Alex <Alex@DESKTOP-5IM2NTD>
2017-09-17 18:47:48 -0400

    Added Log in, Main Menu and Settings UI classes with a basic init components method.

---
+++ src/userinterface/LoginUI.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package userinterface;
+
+/**
+ *
+ * @author Alex
+ */
+
+/**
+     * This is the default constructor for the LoginUI class
+     */
+public class LoginUI {
+    
+
+/**
+ * This method initializes the J components
+ */
+
+private void initComponents() {
+
+}
+
+}
