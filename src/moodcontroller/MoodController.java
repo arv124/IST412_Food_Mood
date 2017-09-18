4140f191057ff3461adbf36331529f5926f14a21
nadaziab <nadaziab@104.38.63.23>
2017-09-17 23:07:17 -0400

    moodmodel and moodcontroller

---
+++ src/moodcontroller/MoodController.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package moodcontroller;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author nadaziab
+ */
+public class MoodController {
+    
+    ArrayList<moodmodel.Mood> moods;
+    
+    /**
+     * Default constructor for MoodController class
+     */
+    public MoodController() {
+        
+    }
+    
+    /**
+     * Add a mood
+     * @param mood the mood to be added
+     */
+    public void addMood(moodmodel.Mood mood) {
+        moods.add(mood);
+    }
+    
+    /**
+     * Change a mood in the list of moods
+     * @param index the index of the mood to be changed
+     * @param mood the mood to be added in place of old mood
+     */
+    public void changeMood(int index, moodmodel.Mood mood) {
+        moods.set(index, mood);
+    }
+    
+    /**
+     * Remove a mood from the list of moods
+     * @param index the index of the mood to be removed
+     */
+    public void removeMood(int index) {
+        moods.remove(index);
+    }
+}
---
+++ src/moodmodel/Mood.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package moodmodel;
+
+import java.util.Date;
+
+/**
+ *
+ * @author nadaziab
+ */
+public class Mood {
+    
+    private String description;
+    private Date recordedAt;
+    
+    /**
+     * The default constructor for the Mood class
+     */
+    public Mood() {
+        
+    }
+    
+    /**
+     * The constructor for the Mood Class with the description
+     * @param description the description of the mood
+     */
+    public Mood(String description, Date recordedAt) {
+        this.description = description;
+        this.recordedAt = recordedAt;
+    }
+    
+    /**
+     * Get the description of the mood
+     * @return the description of the mood
+     */
+    public String getDescription() {
+        return this.description;
+    }
+    
+    /**
+     * Set the description of the mood
+     * @param description the description of the mood
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    /**
+     * Get when the mood was recorded
+     * @return when the mood was recorded
+     */
+    public Date getRecordedAt() {
+        return recordedAt;
+    }
+    
+    /**
+     * Set when the mood was recorded
+     * @param recordedAt when the mood was recorded
+     */
+    public void setRecordedAt(Date recordedAt) {
+        this.recordedAt = recordedAt;
+    }
+}
