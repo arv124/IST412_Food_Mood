30970dc789b6326219119451d66addca864ab564
Alex <Alex@DESKTOP-5IM2NTD>
2017-10-24 17:38:31 -0400

    Added CRUD methods to the database class for Foods and Moods

--- src/databasecontroller/Database.java
+++ src/databasecontroller/Database.java
@@ -21,7 +21,7 @@ public class Database {
     public String url = "https://foodmood-a4f9d.firebaseio.com/";
     
     public Database() {}
-    
+    //*********************************************************************Profile Section********************************************************************
     //This method should take a ProfileModel object.
     public void POSTProfile() throws Exception {
         try {
@@ -106,6 +106,211 @@ public class Database {
     {
     try {
             String url = "https://foodmood-a4f9d.firebaseio.com/profiles/" + username + ".json";
+            URL urlConnect = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) urlConnect.openConnection();
+            con.setDoOutput(true);
+            con.setDoInput(true);
+            con.setRequestProperty("X-HTTP-Method-Override", "PATCH");
+            con.setRequestMethod("POST");
+
+            
+          // User profile info gets translated into JSON to be used in the next line. Use .getname() kinda stuff.
+            String jsonFormattedUserData = new String();
+            
+            
+        } 
+    catch (Exception e) 
+        {
+            e.printStackTrace();
+        }
+    }
+    // **************************************************************FOODS SECTION***************************************************************************
+    public void POSTFood() throws Exception {
+        try {
+            URL urlConnection = new URL(url);
+
+            HttpURLConnection con = (HttpURLConnection) urlConnection.openConnection();
+            con.setDoOutput(true);
+            con.setDoInput(true);
+            con.setRequestProperty("Content-Type", "application/json");
+            con.setRequestProperty("Accept", "application/json");
+            con.setRequestProperty("X-HTTP-Method-Override", "PATCH");
+            con.setRequestMethod("POST");
+// User profile info gets translated into JSON to be used in the next line. Use .getname() kinda stuff.
+            String jsonFormattedUserData = new String();
+
+            //System.out.println("");
+
+            OutputStreamWriter osw = new OutputStreamWriter(con.getOutputStream());
+            osw.write(jsonFormattedUserData);
+            osw.flush();
+            osw.close();
+
+            if (con.getResponseCode() != HttpURLConnection.HTTP_CREATED) {
+                System.out.print("HTTP code : "
+                        + con.getResponseCode());
+            }
+
+        } catch (MalformedURLException e) {
+            System.out.print("URL Malformed");
+        }
+
+    }
+    // This method should return a profile model object.
+    public void readFoodData(String foodID) {
+        String url = "https://foodmood-a4f9d.firebaseio.com/foods/" + foodID + ".json";
+        String inputLine = "";
+        //System.out.println(url);
+        try {
+            URL urlConnect = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) urlConnect.openConnection();
+            con.setDoOutput(true);
+            con.setDoInput(true);
+            con.setRequestMethod("GET");
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+
+            inputLine = in.readLine();
+
+            //System.out.println(inputLine);//testline
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // This will construct a profile model object.
+        //return new ProfileModel();
+    }
+    // this method will take a profilemodel, user data parameter (e.g. weight) and the value of the parameter (e.g. 180)
+    public void updateFoodData(String foodID)
+    {
+    try {
+        String url = "https://foodmood-a4f9d.firebaseio.com/foods/" + foodID + ".json";
+            URL urlConnect = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) urlConnect.openConnection();
+            con.setDoOutput(true);
+            con.setDoInput(true);
+            con.setRequestProperty("X-HTTP-Method-Override", "PATCH");
+            con.setRequestMethod("POST");
+
+            
+          // User profile info gets translated into JSON to be used in the next line. Use .getname() kinda stuff.
+            String jsonFormattedUserData = new String();
+            
+            
+        } 
+    catch (Exception e) 
+        {
+            e.printStackTrace();
+        }
+    
+    }
+    // this method will take a ProfileModel object
+    public void deleteFoodData(String foodID)
+    {
+    try {
+            String url = "https://foodmood-a4f9d.firebaseio.com/foods/" + foodID + ".json";
+            URL urlConnect = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) urlConnect.openConnection();
+            con.setDoOutput(true);
+            con.setDoInput(true);
+            con.setRequestProperty("X-HTTP-Method-Override", "PATCH");
+            con.setRequestMethod("POST");
+
+            
+          // User profile info gets translated into JSON to be used in the next line. Use .getname() kinda stuff.
+            String jsonFormattedUserData = new String();
+            
+            
+        } 
+    catch (Exception e) 
+        {
+            e.printStackTrace();
+        }
+    }
+    
+     // **************************************************************Moods SECTION***************************************************************************
+    public void POSTMood() throws Exception {
+        try {
+            URL urlConnection = new URL(url);
+
+            HttpURLConnection con = (HttpURLConnection) urlConnection.openConnection();
+            con.setDoOutput(true);
+            con.setDoInput(true);
+            con.setRequestProperty("Content-Type", "application/json");
+            con.setRequestProperty("Accept", "application/json");
+            con.setRequestProperty("X-HTTP-Method-Override", "PATCH");
+            con.setRequestMethod("POST");
+// User profile info gets translated into JSON to be used in the next line. Use .getname() kinda stuff.
+            String jsonFormattedUserData = new String();
+
+            //System.out.println("");
+
+            OutputStreamWriter osw = new OutputStreamWriter(con.getOutputStream());
+            osw.write(jsonFormattedUserData);
+            osw.flush();
+            osw.close();
+
+            if (con.getResponseCode() != HttpURLConnection.HTTP_CREATED) {
+                System.out.print("HTTP code : "
+                        + con.getResponseCode());
+            }
+
+        } catch (MalformedURLException e) {
+            System.out.print("URL Malformed");
+        }
+
+    }
+    // This method should return a profile model object.
+    public void readMoodData(String moodID) {
+        String url = "https://foodmood-a4f9d.firebaseio.com/moods/" + moodID + ".json";
+        String inputLine = "";
+        //System.out.println(url);
+        try {
+            URL urlConnect = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) urlConnect.openConnection();
+            con.setDoOutput(true);
+            con.setDoInput(true);
+            con.setRequestMethod("GET");
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+
+            inputLine = in.readLine();
+
+            //System.out.println(inputLine);//testline
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // This will construct a profile model object.
+        //return new ProfileModel();
+    }
+    // this method will take a profilemodel, user data parameter (e.g. weight) and the value of the parameter (e.g. 180)
+    public void updateMoodData(String moodID)
+    {
+    try {
+        String url = "https://foodmood-a4f9d.firebaseio.com/foods/" + moodID + ".json";
+            URL urlConnect = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) urlConnect.openConnection();
+            con.setDoOutput(true);
+            con.setDoInput(true);
+            con.setRequestProperty("X-HTTP-Method-Override", "PATCH");
+            con.setRequestMethod("POST");
+
+            
+          // User profile info gets translated into JSON to be used in the next line. Use .getname() kinda stuff.
+            String jsonFormattedUserData = new String();
+            
+            
+        } 
+    catch (Exception e) 
+        {
+            e.printStackTrace();
+        }
+    
+    }
+    // this method will take a ProfileModel object
+    public void deleteMoodData(String moodID)
+    {
+    try {
+            String url = "https://foodmood-a4f9d.firebaseio.com/foods/" + moodID + ".json";
             URL urlConnect = new URL(url);
             HttpURLConnection con = (HttpURLConnection) urlConnect.openConnection();
             con.setDoOutput(true);
