f10d93c6be1ae19d7995eeb17c2ab0e2023c61bf
nadaziab <nadaziab@104.39.118.151>
2017-10-26 23:29:13 -0400

    Updates to AddFoodUI, LoginViewUI, MainMenuUI, AddMoodUI, and CreateProfileUI. NavigationController added

--- src/profileview/CreateProfileUI.java
+++ src/profileview/CreateProfileUI.java
@@ -14,6 +14,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 import java.awt.event.*;
+import loginview.LoginViewUI;
 import databasecontroller.Database;
 
 /**
